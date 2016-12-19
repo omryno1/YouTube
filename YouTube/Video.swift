e26abe6bd0bcc9412c4fc2274f4e97aa3301501f
Omryno1 <Omryno1@gmail.com>
2016-12-19 12:51:47 +0200

    First Commit

---
+++ YouTube/Video.swift
@@ -0,0 +1,21 @@
+//
+//  Video.swift
+//  YouTube
+//
+//  Created by Omry Dabush on 10/12/2016.
+//  Copyright © 2016 Omry Dabush. All rights reserved.
+//
+
+import UIKit
+
+class Video: NSObject {
+    var thumbnailImageName : String?
+    var videoTitle : String?
+    var numOfViews : NSNumber?
+    var uplodDate : NSData?
+    var videoSubtitle : String?
+    var duration : NSNumber?
+    
+    var channel : Channel?
+     
+}
